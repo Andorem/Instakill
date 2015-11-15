5f1ac6204351381f9be6132c5289080f021f2834
BuildTools <andoremmc2@gmail.com>
2015-11-15 02:02:03 -0800

    v1.1.0 - Now supports multiple items and data values, added list and reload commands

--- src/main/java/io/github/andorem/instakill/Instakill.java
+++ src/main/java/io/github/andorem/instakill/Instakill.java
@@ -1,20 +1,42 @@
 package io.github.andorem.instakill;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Instakill extends JavaPlugin {
 
-	public static File DATA_FOLDER;
+	static File DATA_FOLDER;
 	Logger log = getLogger();
 	ItemListener itemListener;
-
+	
+    String chatHeader = ChatColor.AQUA + "\n======[" + ChatColor.WHITE + getName() + ChatColor.AQUA + "]======\n";
+	String noPermission = ChatColor.RED + "You do not have permission to perform this command.";
+	String listReminder = ChatColor.GOLD + "To list your available Instakill items, type " + ChatColor.GREEN + "/ik list" + ChatColor.GOLD + ".";
+	
+	FileConfiguration generalConfig;
+	
+	boolean debugEnabled = false;
+	
 	@Override
 	public void onEnable() {
 		DATA_FOLDER = getDataFolder();
@@ -23,62 +45,212 @@ public class Instakill extends JavaPlugin {
 			if (!DATA_FOLDER.exists()) {
 				DATA_FOLDER.mkdir();
 			}
-			ensureConfigExists();
+			ensureConfigsExist("config", "items");
 		} catch (Exception e) {
 			e.printStackTrace();
-
 		}
 
+		debugEnabled = getConfig().getBoolean("debug");
+		debug("==============================================================", "INFO");
+		debug("Debug mode activated. Now reporting to console.", "INFO");
+		debug("To disable debugging, set 'debug' in 'config.yml' to false.", "INFO");
+		debug("==============================================================", "INFO");
+		
+		debug("Retrieving config.yml and items.yml from ~/Instakill/", "INFO");
+		generalConfig = getConfig();
+		
 		itemListener = new ItemListener(this);
-
 		getServer().getPluginManager().registerEvents(itemListener, this);
+		debug("PlayerInteractEvent ItemListener registered.", "INFO");
 
 	}
 
-	private void ensureConfigExists() {
-		File file = new File(DATA_FOLDER, "config.yml");
-		if (!file.exists()) {
-			log.info("No config.yml found. Generating default one.");
-			saveDefaultConfig();
-			reloadConfig();
-		}
-	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-
-		boolean senderIsPlayer = (sender instanceof Player);
-		Player pSender = (senderIsPlayer ? (Player) sender : null);
-
 		if (cmd.getName().equalsIgnoreCase("ik")) {
-			if (args.length > 0) {
-				if (args[0].equalsIgnoreCase("give")) {
+			Player pSender = ((sender instanceof Player) ? (Player) sender : null);
+			String senderName = sender.getName();
+			if (args.length == 0) {
+				if (sender.hasPermission("instakill.list")) sender.sendMessage(listReminder);
+				if (sender.hasPermission("instakill.give")) return false;
+			}
+			else {
+				
+				if (args[0].equalsIgnoreCase("list")) {
+					debug("FROM " + senderName + ": Requested Instakill item list", "INFO");
+			
+					if (sender.hasPermission("instakill.list")) {
+						debug("TO " + senderName + ": Sending Instakill item list", "INFO");
+						Set<String> allowedKillItems = itemListener.allowedKillItemsFor(pSender);
+						String itemsList = itemListener.getItemsList(allowedKillItems, itemListener.getItemConfig());
+						
+						sender.sendMessage(chatHeader);
+						if (sender.hasPermission("instakill.give")) {
+							sender.sendMessage(ChatColor.WHITE + "To give an item, type /ik give [user] " + ChatColor.GREEN 
+								+ "[" + ChatColor.GOLD + "item name" + ChatColor.GREEN + "]" + ChatColor.WHITE + " (amount)\n" + ChatColor.GRAY); 
+						}
+						sender.sendMessage(ChatColor.WHITE + "You can use the following Instakill items:");
+						sender.sendMessage(itemsList);
+						
+						debug("TO " + senderName + ": Instakill item list sent", "INFO");
+					}
+					else { 
+						sender.sendMessage(noPermission);
+						debug("FROM " + senderName + ": Does not have the correct permission (instakill.list)!", "WARNING");
+					}
+				}
+				
+				else if (args[0].equalsIgnoreCase("give")) {
+					debug("FROM " + senderName + ": Initiated command to give an Instakill item", "INFO");
 					if (sender.hasPermission("instakill.give")) {
-						if (!senderIsPlayer) {
-							sender.sendMessage("You must be a player to use that command.");
-						} 
+						if (args.length < 3) {
+							sender.sendMessage(listReminder);
+							debug("FROM " + senderName + ": Not enough arguments for give command (at least three)", "INFO");
+							return false;
+						}
 						else {
-							Player recipient = args.length > 1 ? getServer().getPlayer(args[1]) : pSender;
-							if (recipient != null) {
-								int numberOfItems;
-								try {
-									numberOfItems = (args.length == 3 ? Integer.parseInt(args[2]) : 1);
-								}
-								catch (NumberFormatException e) {
-								  return false;
-								}
-								recipient.getInventory().addItem(itemListener.createKillItem(numberOfItems));
+						
+							String itemSectionName = "";
+							int numberOfItems = 1;
+							Player recipient = getServer().getPlayer(args[1]);
+							debug("FROM " + senderName + ": Attempting to get player " + args[1] + " from server...", "INFO");
+							
+							if (recipient == null) {
+								sender.sendMessage(ChatColor.RED + "Error: " + ChatColor.DARK_RED + "Player not found.");
+								debug("TO " + senderName + ": Player " + args[1] + " returned NULL", "WARNING");
 							}
 							else {
-								sender.sendMessage(ChatColor.RED + "Error: " + ChatColor.DARK_RED + "Player not found.");
+								debug("FROM " + senderName + ": Player " + args[1] + " found.", "INFO");
+								itemSectionName = args[2].toLowerCase();
+								if ((args.length == 4)) {
+									numberOfItems = (isInt(args[3]) ? Integer.parseInt(args[3]) : 1);
+								}
+								debug("FROM " + senderName + ": Attempting to give " + numberOfItems + " of " + itemSectionName + " to " + recipient.getName(), "INFO");
+							
+							
+								if (itemListener.getKillItemSectionNames().contains(itemSectionName)) {
+									String itemName = itemListener.getItemConfig().getString(itemSectionName + ".name");
+									debug("FROM " + senderName + ": " + itemName + " found in 'items.yml' under section title '" + itemSectionName + "'", "INFO");
+									
+									ItemStack killItem = itemListener.createKillItem(itemSectionName, numberOfItems);
+									if (killItem == null) {
+										sender.sendMessage(ChatColor.RED + "Error: " + ChatColor.DARK_RED + "Could not create Instakill item from specified ID.");
+										debug("FROM " + senderName + ": " + "FAILED to create item '" + itemName + "'.", "WARNING");
+									}
+									recipient.getInventory().addItem(killItem);
+									sender.sendMessage(ChatColor.GOLD + "Giving " + ChatColor.RED + numberOfItems +ChatColor.GOLD + " of " 
+										+ ChatColor.RED + itemName + ChatColor.GOLD + " to " 
+										+ ChatColor.DARK_RED + recipient.getName() + ChatColor.GOLD + ".");
+									debug("FROM " + senderName + ": Giving " + numberOfItems + " of " + itemName + " to " + recipient.getName(), "INFO");
+								}
+								else {
+									sender.sendMessage(ChatColor.RED + "This Instakill item does not exist!");
+									if (sender.hasPermission("instakill.list")) sender.sendMessage(listReminder);
+									debug("FROM " + senderName + ": Instakill item '" + itemSectionName + "' not found in items.yml", "WARNING");
+								}
 							}
 						}
-					} 
-					else
-						sender.sendMessage("You do not have permission to perform this command.");
+					}
+					else {
+						sender.sendMessage(noPermission);
+						debug("FROM " + senderName + ": Does not have the correct permission (instakill.give)!", "WARNING");
+					}
+				}
+				
+				else if (args[0].equalsIgnoreCase("reload")) {
+					debug("FROM " + senderName + ": Sent command to reload all configurations.", "INFO");
+					
+					if (sender.hasPermission("instakill.reload")) {
+						debug("FROM " + senderName + ": Reloading configuration files.", "INFO");
+						reloadConfig();
+						updateFromConfig();
+						debug("TO " + senderName + ": Configuration 'config.yml' reloaded.", "INFO");
+						
+						itemListener.updateFromConfig(loadConfig("items"));
+						debug("TO " + senderName + ": Configuration 'items.yml' reloaded.", "INFO");
+						
+						sender.sendMessage(ChatColor.GREEN + "Configs reloaded.");
+					}
+					else {
+						sender.sendMessage(noPermission);
+						debug("FROM " + senderName + ": Does not have the correct permission (instakill.reload)!", "WARNING");
+					}
 				}
 			}
 		}
 		return true;
 	}
+	
+	FileConfiguration loadConfig(String configName) {
+		FileConfiguration loadedConfig;
+		ensureConfigsExist(configName);
+		debug("Loading configuration file '" + configName + ".yml'...", "INFO");
+		loadedConfig = YamlConfiguration.loadConfiguration(new File(DATA_FOLDER, configName + ".yml"));
+		debug("Configuration '" + configName + ".yml' loaded.", "INFO");
+		return loadedConfig;
+	}
+	
+	void saveConfig(String configName, FileConfiguration config) {
+		File file = new File(DATA_FOLDER, configName + ".yml");
+		try {
+			config.save(file);
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+			log.severe("Failed to save configuration to '"+ configName + ".yml'! Does it exist?");
+		}
+	}
+	
+	
+	void saveDefaultConfig(String configName) {
+		saveResource(configName + ".yml", false);
+    }
+	
+	boolean configExists(String configName) {
+		File file = new File(DATA_FOLDER, configName + ".yml");
+		return file.exists();
+	}
+	
+	void ensureConfigsExist(String... configNames) {
+		for (String name : configNames) {
+			if (!configExists(name)) {
+				log.info("No " + name + ".yml found. Generating default one.");
+				if (name.equals("config")) {
+					saveDefaultConfig();
+					reloadConfig();
+				}
+				else {
+					saveDefaultConfig(name);
+				}
+			}
+		}
+	}
+	
+	void updateFromConfig() {
+		generalConfig = getConfig();
+		debugEnabled = generalConfig.getBoolean("debug");
+	}
+	
+
+	
+	boolean isInt(String s) {
+		try {
+			int num = Integer.parseInt(s);
+			return true;
+		}
+		catch (NumberFormatException e) {
+			return false;
+		}
+	}
+	
+	
+	
+	void debug(String debugMessage, String levelName) {
+		Level level = Level.parse(levelName.toUpperCase());
+		if (debugEnabled) {
+			log.log(level, "[DEBUG] " + debugMessage);
+		}
+	}
+	
 }
--- src/main/java/io/github/andorem/instakill/ItemListener.java
+++ src/main/java/io/github/andorem/instakill/ItemListener.java
@@ -1,10 +1,13 @@
 package io.github.andorem.instakill;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Creature;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
@@ -19,58 +22,212 @@ import org.bukkit.inventory.meta.ItemMeta;
 public class ItemListener implements Listener {
 
 	Instakill plugin;
-	Material killItem;
-	int killItemID;
-	List<String> killItemLore;
-	String killItemName;
-	double killRadius;
-	boolean mobDropsEnabled;
-	boolean isConsumable;
+	Set<String> killItemSectionNames;
+	Set<String> killItemIDs = new HashSet<String>();
+	FileConfiguration itemConfig, generalConfig;
 
 	public ItemListener(Instakill plugin) {
+		plugin.debug("Creating new ItemListener...", "INFO");
 		this.plugin = plugin;
-		FileConfiguration config = plugin.getConfig();
-		killItemID = config.getInt("item-id");
-		killItem = Material.getMaterial(killItemID);
-		killItemName = config.getString("item-name");
-		killItemLore = config.getStringList("item-lore");
-		killRadius =config .getDouble("kill-radius");
-		mobDropsEnabled = config.getBoolean("mob-drops");
-		isConsumable = config.getBoolean("consume-item");
+		generalConfig = plugin.getConfig();
+		updateFromConfig(plugin.loadConfig("items"));
 	}
 
+	Set<String> getKillItemSectionNames() {
+		return killItemSectionNames;
+	}
+	
+	Set<String> getKillItemIDs() {
+		return killItemIDs;
+	}
+	
+	Set<String> updateKillItemIDs(Set<String> itemSectionNames) {
+		Set<String> updatedIDs = new HashSet<String>();
+		plugin.debug("Retrieving item IDs for all items...", "INFO");
+		for (String sectionName: itemSectionNames) {
+			plugin.debug("Getting ID for item " + sectionName, "INFO");
+			try {
+				String itemID = itemConfig.getString(sectionName + ".id");
+				updatedIDs.add(itemID);
+				plugin.debug("Item ID " + itemID + " found", "INFO");
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+				plugin.debug("Could not find item ID for " + sectionName, "WARNING");
+			}
+		}
+		plugin.debug("Instakill item IDs retrieved.", "INFO");
+		return updatedIDs;
+	}
+	
 	@EventHandler
 	public void onPlayerUse(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
-		if (player.hasPermission("instakill.use")) {
-			if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
-				ItemStack itemInHand = player.getItemInHand();
-
-				if (itemInHand.getType() == killItem && itemInHand.hasItemMeta()) {
-					ItemMeta itemMeta = itemInHand.getItemMeta();
-					if (itemMeta.getDisplayName().equals(killItemName) && itemMeta.getLore().equals(killItemLore)) {
+		String playerName = player.getName();
+		plugin.debug("PLAYER " + playerName + ": Has access to Instakill items. Watching.", "INFO");
+		if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+			ItemStack itemInHand = player.getItemInHand();
+			String itemType = Integer.toString(itemInHand.getTypeId());
+			String itemDataValue = Short.toString(itemInHand.getDurability());
+			String itemID = (itemDataValue.equals("0") ? itemType : (itemType + ":" + itemDataValue));
+			plugin.debug("PLAYER " + playerName + ": Right-clicked with item " + itemID, "INFO");
+			if (itemInHand.hasItemMeta()) {
+				plugin.debug("PLAYER " + playerName + ": Item has metadata information. Checking if Instakill item...", "INFO");
+				ItemMeta itemMeta = itemInHand.getItemMeta();
+				String itemDisplayName = itemMeta.getDisplayName();
+				List<String> itemLore = itemMeta.getLore();
+				ConfigurationSection killItemSection = getKillItemSection(itemID, itemDisplayName, itemLore);
+				if (killItemSection != null) {
+					event.setCancelled(true);
+					plugin.debug("PLAYER " + playerName + ": Item ID found in items.yml. Item has custom diplay name '" + itemDisplayName + "' and lore '" + itemLore.toString() + "'", "INFO");
+					String itemPerm = killItemSection.getName().toLowerCase();
+					if (player.hasPermission("instakill.use." + itemPerm) || player.hasPermission("instakill.use.*")) {
+						double killRadius = generalConfig.getDouble("kill-radius");
+						if (killItemSection.contains("kill-radius")) killRadius = killItemSection.getDouble("kill-radius"); 
+						
+						boolean mobDropsEnabled = generalConfig.getBoolean("mob-drops");
+						if (killItemSection.contains("mob-drops")) mobDropsEnabled = killItemSection.getBoolean("mob-drops");
+						
+						boolean isConsumable = generalConfig.getBoolean("consume-item");
+						if (killItemSection.contains("consume-item")) isConsumable = killItemSection.getBoolean("consume-item");
+						
 						List<Entity> nearbyEntities = player.getNearbyEntities(killRadius, killRadius, killRadius);
+						int killedEntities = 0;
 						for (Entity entity : nearbyEntities) {
 							if (entity instanceof LivingEntity && !(entity instanceof Player)) {
 								Damageable mob = (Damageable) entity;
 								if (!mobDropsEnabled) mob.setHealth(0);
 								else mob.damage(99999999);
+								killedEntities++;
 							}
 						}
-						if (isConsumable) player.getInventory().remove(itemInHand);
-						event.setCancelled(true);
+						plugin.debug("PLAYER " + playerName + ": Killed " + killedEntities + " entities (out of " + nearbyEntities.size() + ") within " + killRadius + " blocks of player.", "INFO");
+						if (isConsumable) {
+							itemInHand.setAmount(itemInHand.getAmount() - 1);
+							player.setItemInHand(itemInHand);
+							plugin.debug("PLAYER " + playerName + ": 1 of Instakill item consumed.", "INFO");
+						}
+						else {
+							player.setItemInHand(itemInHand);
+						}
 					}
+					else {
+						player.sendMessage(ChatColor.RED + "You do not have permission to use this item!");
+						plugin.debug("PLAYER " + playerName + ": Does not have permission to use this Instakill item! (instakill.use." + itemPerm + ")", "WARNING");
+					}
+					
+				}
+				else {
+					plugin.debug("PLAYER " + playerName + ": No Instakill item match found. Ignoring.", "INFO");
 				}
 			}
 		}
+		else {
+			plugin.debug("PLAYER " + playerName + ": Not right-clicking with any item. Ignoring.", "INFO");
+		}
 	}
 	
-	public ItemStack createKillItem(int itemAmount) {
-		ItemStack newItem = new ItemStack(killItem, itemAmount);
+	ItemStack createKillItem(String sectionName, int itemAmount) {
+		plugin.debug("Creating new ItemStack for " + itemAmount + " of item " + sectionName, "INFO");
+		
+		plugin.debug("Loading item section '" + sectionName + "' from 'items.yml'...", "INFO");
+		ConfigurationSection itemSection = itemConfig.getConfigurationSection(sectionName);
+		if (itemSection == null) {
+			plugin.debug("Item " + sectionName + " not found in 'items.yml'! Use /ik list to get valid item names.", "WARNING");
+			return null;
+		}
+		
+		plugin.debug("Getting item type and data value from '" + sectionName + "'...", "INFO");
+		String[] itemData = itemSection.getString("id").split(":");
+		if (itemData == null) {
+			plugin.debug("Item ID for " + sectionName + " not found! Is 'id: [ID #]' set in 'items.yml'?", "WARNING");
+			return null;
+		}
+		
+		Material itemType = Material.getMaterial(Integer.parseInt(itemData[0]));
+		if (itemType == null) {
+			plugin.debug("Could not match specified item type ('" + itemData[0] + "') to Minecraft item!", "WARNING");
+			return null;
+		}
+		
+		short dataValue = ((itemData.length > 1) ? Short.parseShort(itemData[1]) : 0);
+		plugin.debug("Using item " + itemData[0] + ":" + dataValue, "INFO");
+		
+		ItemStack newItem = new ItemStack(itemType, itemAmount, dataValue);
 		ItemMeta itemMeta = newItem.getItemMeta();
-		itemMeta.setDisplayName(killItemName);
-		itemMeta.setLore(killItemLore);
+		String itemDisplayName = itemSection.getString("name");
+		List<String> itemLore = itemSection.getStringList("lore");
+		if (itemDisplayName == null || itemLore.isEmpty()) {
+			plugin.debug("Could not find custom name and/or lore for " + sectionName + "! Is it set in 'items.yml'?", "WARNING");
+			return null;
+		}
+		itemMeta.setDisplayName(itemDisplayName);
+		itemMeta.setLore(itemLore);
 		newItem.setItemMeta(itemMeta);
+		plugin.debug("ItemStack for " + itemDisplayName + " created.", "INFO");
 		return newItem;
+	}
+	
+	ConfigurationSection getKillItemSection(String itemID, String itemDisplayName, List<String> itemLore) {
+		plugin.debug("Searching for information of '" + itemDisplayName + "' in 'items.yml'...", "INFO");
+		for (String sectionName : killItemSectionNames) {
+			plugin.debug("Matching '" + itemDisplayName + "' against section " + sectionName + "...", "INFO");
+			ConfigurationSection itemSection = itemConfig.getConfigurationSection(sectionName);
+			if (itemSection == null) {
+				plugin.debug("Could not find section '" + sectionName + " in 'items.yml'!", "WARNING");
+				return null;
+			}
+			if (itemSection.getString("id").equals(itemID)
+			  && itemSection.getString("name").equals(itemDisplayName)
+			  && itemSection.getStringList("lore").equals(itemLore)) {
+				plugin.debug("Match found for " + itemDisplayName + " (id = " + itemID +")!", "INFO");
+				return itemSection;
+			}
+		}
+		return null;
+	}
+
+	String getItemsList(Set<String> itemSectionNames, FileConfiguration config) {
+		plugin.debug("Creating list of Instakill items", "INFO");
+		String itemsList = ChatColor.GRAY + "";
+		int i = 0;
+		for (String itemPath : itemSectionNames) {
+			i++;
+			String itemDisplayName = config.getString(itemPath + ".name");
+			String itemEntry = i + ". " + ChatColor.GOLD + itemPath + ChatColor.GRAY + " - " + itemDisplayName + "\n";
+			itemsList += itemEntry;
+			plugin.debug("Adding to list: '" + itemDisplayName + "' from '" + itemPath + "' in 'items.yml'", "INFO");
+		}
+		plugin.debug("List of Instakill items created, with " + i + " entries", "INFO");
+		return itemsList;
+	}
+	
+	Set<String> allowedKillItemsFor(Player player) {
+		if (player == null) {
+			return killItemSectionNames;
+		}
+		else {
+			String playerName = player.getName();
+			Set<String> allowedItems = new HashSet<String>();
+			for (String itemPath : killItemSectionNames) {
+				if (player.hasPermission("instakill.use." + itemPath)) {
+					allowedItems.add(itemPath);
+					plugin.debug("PLAYER " + playerName + ": Has permission instakill.use." + itemPath, "INFO");
+				}
+			}
+			return allowedItems;
+		}
+	}
+	
+	void updateFromConfig(FileConfiguration newConfig) {
+		plugin.debug("Updating Instakill item information...", "INFO");
+		itemConfig = newConfig;
+		killItemSectionNames = itemConfig.getKeys(false);
+		killItemIDs = updateKillItemIDs(killItemSectionNames);
+		plugin.debug("Instakill items information updated.", "INFO");
+	}
+	
+	FileConfiguration getItemConfig() {
+		return itemConfig;
 	}
 }
